b6ff50c1343bb14cc0e4040f51b10e3a014dfec4
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-26 07:37:07 +0500

    dbd

--- gilimbaaf/src/App.jsx
+++ gilimbaaf/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Landing from './Pages/landing/Landing'
-import Welcome from './Components/welcome/Welcome'
 
 function App() {
 
