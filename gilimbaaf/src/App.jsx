3ec631abdaac3403a5040aab10b75f590d257f07
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-26 07:41:36 +0500

    initial commit 2.0

--- gilimbaaf/src/App.jsx
+++ gilimbaaf/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Landing from './Pages/landing/Landing'
 
 function App() {
 
   return (
     <>
-      
+      <Landing />
     </>
   )
 }
