755a4011fa69db762dadab79b8102380d9736212
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-27 07:01:40 +0500

    fgggn

--- gilimbaaf/src/Pages/Landing/Landing.jsx
+++ gilimbaaf/src/Pages/Landing/Landing.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import './Landing.css';
 import Navbar from '../../Components/Navbar/Navbar';
 import Home from '../../Sections/Home/Home';
-import About from '../../Sections/About/About';
 
 const Landing = () => {
-  const [activeTab, setActiveTab] = React.useState('Home');
   return (
       <div className='landing-container'>
-      <Navbar activeTab={activeTab}/>
-      <Home className={activeTab === 'Home' ? 'active' : ''} />
-      <About className={activeTab === 'About' ? 'active' : ''}/>
+      <Navbar />
+      <Home />
     </div>
   )
 }
