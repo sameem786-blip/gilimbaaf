7f16664606c1d595bc236ac4005e3cff07ce0d71
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-30 06:30:37 +0500

    nlkmnkl

--- gilimbaaf/src/Sections/Home/Home.jsx
+++ gilimbaaf/src/Sections/Home/Home.jsx
@@ -4,7 +4,10 @@ import './Home.css'
 const Home = () => {
   return (
       <div className='home-container'>
-          Yellow
+          <div className="home-row home-row-1"></div>
+          <div className="home-row home-row-2"></div>
+          <div className="home-row home-row-3"></div>
+          <div className="home-row home-row-4"></div>
     </div>
   )
 }
