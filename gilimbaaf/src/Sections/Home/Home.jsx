6085aee09fc581596ed309be50fc740abf560a02
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-31 02:28:30 +0500

    fvefv

--- gilimbaaf/src/Sections/Home/Home.jsx
+++ gilimbaaf/src/Sections/Home/Home.jsx
@@ -4,7 +4,8 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      
+      <div className="parent-column parent-column-1"></div>
+      <div className="parent-column parent-column-2"></div>
     </div>
   );
 };
