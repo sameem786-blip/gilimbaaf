5fe6ef3459597ea0bd3d2d5bbdb3ce1bdaab00bb
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-30 06:38:31 +0500

    kjnbkj

--- gilimbaaf/src/Sections/Home/Home.jsx
+++ gilimbaaf/src/Sections/Home/Home.jsx
@@ -3,11 +3,14 @@ import './Home.css'
 
 const Home = () => {
   return (
-      <div className='home-container'>
-          <div className="home-row home-row-1"></div>
-          <div className="home-row home-row-2"></div>
-          <div className="home-row home-row-3"></div>
-          <div className="home-row home-row-4"></div>
+    <div className='home-container'>
+      <div className="home-row-1-2-col-1">
+Row 1 col-1
+      </div>
+      <div className="home-row-1-col-2">
+
+      </div>
+
     </div>
   )
 }
