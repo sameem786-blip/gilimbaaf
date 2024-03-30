e07357489c8f8bd26cd5579945a3c21bcce1a4a7
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-31 03:01:36 +0500

    fgnfg

--- gilimbaaf/src/Pages/Landing/Landing.jsx
+++ gilimbaaf/src/Pages/Landing/Landing.jsx
@@ -1,23 +1,11 @@
-import React, {useRef,useEffect} from 'react'
+import React from 'react'
 import './Landing.css';
 import Navbar from '../../Components/Navbar/Navbar';
 import Home from '../../Sections/Home/Home';
-import { Application } from '@splinetool/runtime';
 
 const Landing = () => {
-
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const app = new Application(canvas);
-    app.load('https://prod.spline.design/JCLGPA01rYp6YBVC/scene.splinecode');
-  }, []);
-
   return (
       <div className='landing-container'>
-      <Navbar />
-      {/* <canvas ref={canvasRef} id="canvas3d" style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: -1 }} /> */}
       <Home />
     </div>
   )
