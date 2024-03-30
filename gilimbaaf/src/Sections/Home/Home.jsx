a9c2d97a75a18263ddf3a7bb2218fea2c91671e5
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-31 02:30:58 +0500

    rgreg

--- gilimbaaf/src/Sections/Home/Home.jsx
+++ gilimbaaf/src/Sections/Home/Home.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import './Home.css';
+import React from 'react'
+import './Home.css' 
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="parent-column parent-column-1"></div>
-      <div className="parent-column parent-column-2"></div>
-    </div>
-  );
-};
+    <div className='home-container'>
+      <div className="home-row-1-2-col-1">
+        div.
+      </div>
+      <div className="home-row-1-col-2-column">
+        <div className="row-1-col-2-row">
+          <div className="home-col-2-row-1-col-1"></div>
+          <div className="home-col-2-row-1-col-2"></div>
+        </div>
+        <div className="row-1-col-2-row">
+          <div className="home-col-2-row-1-col-1-2"></div>
+          <div className="home-col-2-row-1-col-2-2"></div>
+        </div>
+      </div>
+      
 
-export default Home;
+    </div>
+  )
+}
+
+export default Home
