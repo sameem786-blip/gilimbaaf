fe452b798c9b72879b37577ca57a9c3db97beec3
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-30 06:46:19 +0500

    nkjljnk

--- gilimbaaf/src/Sections/Home/Home.jsx
+++ gilimbaaf/src/Sections/Home/Home.jsx
@@ -8,7 +8,12 @@ const Home = () => {
 Row 1-2 col-1
       </div>
       <div className="home-row-1-col-2">
-Row 1 col-1
+<div className="home-col-2-row-1-col-1"></div>
+<div className="home-col-2-row-1-col-2"></div>
+      </div>
+      <div className="home-row-2-col-2">
+<div className="home-col-2-row-1-col-1"></div>
+<div className="home-col-2-row-1-col-2"></div>
       </div>
 
     </div>
